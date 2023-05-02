d33e7c02c650dedff5073c453ff1030c1c1c11f8
Carlos Moraes <carloshrmoraes@gmail.com>
2023-05-02 11:03:44 -0300

    renomeando o projeto e implementando firebase auth

---
+++ WATER NOW/waternow/app/src/main/java/app/example/waternow/DashBoard.java
@@ -0,0 +1,47 @@
+package app.example.waternow;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.View;
+
+import app.example.waternow.R;
+
+public class DashBoard extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_board);
+
+        drawerLayout=findViewById(R.id.drawer_layout);
+
+    }
+
+    public void ClickMenu(View view){
+        MainActivity.openDrawer(drawerLayout);
+    }
+    public void ClickLogo(View view){
+        MainActivity.closeDrawer(drawerLayout);
+    }
+    public  void Clickhome(View view){
+        MainActivity.redirectActivity(this,MainActivity.class);
+    }
+    public  void clickDashboard(View view){
+        recreate();
+    }
+    public void clickAboutUs(View view){
+        MainActivity.redirectActivity(this,AboutUs.class);
+    }
+    public  void ClickLogout(View view){
+        MainActivity.logout(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        MainActivity.closeDrawer(drawerLayout);
+    }
+}
---
+++ WATER NOW/waternow/app/src/main/java/app/example/waternow/MainActivity.java
@@ -0,0 +1,149 @@
+package app.example.waternow;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import app.example.waternow.R;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(new FirebaseAuthUIActivityResultContract(), new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+        @Override
+        public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+            onSignInResult(result);
+        }
+    });
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawerLayout = findViewById(R.id.drawer);
+        System.out.println("começa");
+        ((Button) findViewById(R.id.btnTest)).setOnClickListener(e -> {
+            List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.GoogleBuilder().build());
+            Intent signInIntent = AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build();
+            signInLauncher.launch(signInIntent);
+        });
+    }
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            Log.i("user", user.getDisplayName());
+        } else {
+            Log.i("user", "nao deu certo");
+        }
+    }
+
+
+    public void ClickMenu(View view) {
+        openDrawer(drawerLayout);
+    }
+
+    public static void openDrawer(DrawerLayout drawerLayout) {
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public void ClickLogo(View view) {
+        closeDrawer(drawerLayout);
+    }
+
+    public static void closeDrawer(DrawerLayout drawerLayout) {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    public void Clickhome(View view) {
+        recreate();
+    }
+
+    public void clickDashboard(View view) {
+        redirectActivity(this, DashBoard.class);
+    }
+
+    public void Notification1(View view) {
+        redirectActivity(this, Notificacao.class);
+    }
+
+    public void Conta1(View view) {
+        redirectActivity(this, Conta.class);
+    }
+
+    public void Relatorio1(View view) {
+        redirectActivity(this, Relatorio.class);
+    }
+
+    public void Metas1(View view) {
+        redirectActivity(this, Metas.class);
+    }
+
+    public void Configuracoes1(View view) {
+        redirectActivity(this, Configuracoes.class);
+    }
+
+    public void clickAboutUs(View view) {
+        redirectActivity(this, AboutUs.class);
+    }
+
+    public void ClickLogout(View view) {
+        logout(this);
+    }
+
+    public static void logout(Activity activity) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setTitle("Logout");
+        builder.setMessage("Are you sure you want to logout?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                activity.finishAffinity();
+                System.exit(0);
+            }
+        });
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+    public static void redirectActivity(Activity activity, Class Class) {
+        Intent intent = new Intent(activity, Class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeDrawer(drawerLayout);
+    }
+}

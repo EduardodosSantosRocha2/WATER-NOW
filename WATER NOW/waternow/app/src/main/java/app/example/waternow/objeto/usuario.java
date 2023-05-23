1b3e3f1ef9a9718c95c081759bb0ef7cd864f5e8
Vanessa Machado Silva <lampetrolaboratorioufu@gmail.com>
2023-05-23 14:18:43 -0300

    Classe e telaCadastro
    Classe usuario e tela de cadastro

---
+++ WATER NOW/waternow/app/src/main/java/app/example/waternow/objeto/usuario.java
@@ -0,0 +1,31 @@
+package app.example.waternow.objeto;
+
+public class usuario {
+    public float peso;
+    public float altura;
+    public String sexo;
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+}

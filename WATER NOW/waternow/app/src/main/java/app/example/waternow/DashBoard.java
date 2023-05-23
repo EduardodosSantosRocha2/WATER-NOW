6a5ba3facd472c80ecee56e7cc76fc67446e04a6
Carlos Moraes <carloshrmoraes@gmail.com>
2023-05-23 14:08:37 -0300

    teste firebase cloud database

--- WATER NOW/waternow/app/src/main/java/app/example/waternow/DashBoard.java
+++ WATER NOW/waternow/app/src/main/java/app/example/waternow/DashBoard.java
@@ -1,14 +1,20 @@
 package app.example.waternow;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import com.google.firebase.auth.FirebaseUser;
-
-import app.example.waternow.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class DashBoard extends AppCompatActivity {
 
@@ -18,6 +24,20 @@ public class DashBoard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
         drawerLayout=findViewById(R.id.drawer_layout);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Pessoa teste = new Pessoa(FirebaseAuth.getInstance().getCurrentUser().getUid(), "Joao Teste");
+
+        ((Button)findViewById(R.id.btnAddAgua)).setOnClickListener((e) ->
+        {
+            teste.agua = Double.parseDouble(((EditText) findViewById(R.id.editQtdAgua)).getText().toString());
+
+            db.collection("pessoa")
+                    .add(teste)
+                    .addOnSuccessListener(documentReference -> Log.d("a", "DocumentSnapshot written with ID: " + documentReference.getId()))
+                    .addOnFailureListener(e1 -> Log.w("a", "Error adding document", e1));
+        });
     }
     public void Conta1(View view) {
         MainActivity.redirectActivity(this, Conta.class);

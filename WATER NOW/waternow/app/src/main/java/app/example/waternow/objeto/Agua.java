b434ac9ea9dbc0f51a9f1903c40f280aa67c05c7
Carlos Moraes <carloshrmoraes@gmail.com>
2023-05-29 15:04:16 -0300

    modelo para agua

--- WATER NOW/waternow/app/src/main/java/app/example/waternow/objeto/Agua.java
+++ WATER NOW/waternow/app/src/main/java/app/example/waternow/objeto/Agua.java
@@ -1,6 +1,18 @@
 package app.example.waternow.objeto;
 
+import com.google.type.DateTime;
+
 public class Agua {
     public String id_usuario;
-    public
+    public DateTime data;
+    public Float agua;
+
+    public Agua() {
+    }
+
+    public Agua(String id_usuario, DateTime data, Float agua) {
+        this.id_usuario = id_usuario;
+        this.data = data;
+        this.agua = agua;
+    }
 }
